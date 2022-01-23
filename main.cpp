7f92842a6f6234b2f627a4af6fe2e85871487d9d
hawkdeath <hawkdeath@pop-os.localdomain>
2022-01-23 10:56:16 +0100

    update cmake version, update a main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include "MineVoxelGame.h"
 #include "Log.h"
-#include <memory>
+
 
 int main(int argc, char* argv[]) {
  
   try {
-    std::unique_ptr<mv::MineVoxelGame> game = std::make_unique<mv::MineVoxelGame>();
-    game->run();
+    mv::MineVoxelGame game;
+    game.run();
   }
   catch (std::exception& e) {
     ELOG(e.what());
