715b08208b00720dc3a6314c211b53a5b21d8395
hawkdeath <patryj1083@gmail.com>
2022-02-02 12:07:12 +0100

    added buffer class

--- include/Buffer.h
+++ include/Buffer.h
@@ -5,23 +5,60 @@
 #include <vulkan/vulkan.h>
 
 namespace mv {
-  class Buffer
-  {
-  public:
-    Buffer(Device& device);
-    ~Buffer();
+	class Buffer
+	{
+	public:
+		Buffer(Device& device, VkDeviceSize instanceSize, uint32_t instanceCount,
+			VkBufferUsageFlags usageFlags, VkMemoryPropertyFlags memPropFlags,
+			VkDeviceSize minOffsetAlignment = 1);
+		~Buffer();
 
-  private:
-    Device& mDevice;
+		Buffer(const Buffer&) = delete;
+		Buffer& operator=(const Buffer&) = delete;
 
-  };
 
-  Buffer::Buffer(Device& device) : mDevice{ device }
-  {
-  }
+		VkResult map(VkDeviceSize size, VkDeviceSize offset = 0);
+		void unMap();
 
-  Buffer::~Buffer()
-  {
-  }
+		void writeToBuffer(void* data, VkDeviceSize size, VkDeviceSize offset = 0);
+		VkResult flush(VkDeviceSize size, VkDeviceSize offset = 0);
+		VkDescriptorBufferInfo descriptorInfo(VkDeviceSize size, VkDeviceSize offset = 0);
+		VkResult invalidate(VkDeviceSize size, VkDeviceSize offset = 0);
+
+		void writeToIndex(void* data, int32_t idx);
+		VkResult flushToIndex(int32_t idx);
+		VkDescriptorBufferInfo descriptorInfoIndex(int32_t idx);
+		VkResult invalidateIndex(int32_t idx);
+
+		void* getMappedMemory() const { return mMapped; }
+		VkBuffer getBuffer() const { return mBuffer; }
+		VkDeviceMemory getMemory() const { return mMemory; }
+
+		VkDeviceSize getBufferSize() const { return mBufferSize; }
+		uint32_t getInstanceCount() const { return mInstanceCount; }
+		VkDeviceSize getInstanceSize() const { return mInstanceSize; }
+		VkDeviceSize getAlignmentSize() const { return mAlignmentSize; }
+		VkBufferUsageFlags getUsageFlags() const { return mUsageFlags; }
+		VkMemoryPropertyFlags getMemoryPropertyFlags() const { return mMemoryPropertyFlags; }
+
+
+	private:
+		static VkDeviceSize getAlignment(VkDeviceSize instanceSize, VkDeviceSize minOffsetAlignment);
+
+	private:
+		Device& mDevice;
+
+		void* mMapped = nullptr;
+		VkBuffer mBuffer;
+		VkDeviceMemory mMemory;
+
+		VkDeviceSize mBufferSize;
+		uint32_t mInstanceCount;
+		VkDeviceSize mInstanceSize;
+		VkDeviceSize mAlignmentSize;
+		VkBufferUsageFlags mUsageFlags;
+		VkMemoryPropertyFlags mMemoryPropertyFlags;
+	};
+
 
 }
--- src/Buffer.cpp
+++ src/Buffer.cpp
@@ -1,6 +1,106 @@
 #include "Buffer.h"
 
+#include <cassert>
+#include <cstring>
+
 namespace mv {
 
-    
+	Buffer::Buffer(Device& device, VkDeviceSize instanceSize, uint32_t instanceCount,
+		VkBufferUsageFlags usageFlags, VkMemoryPropertyFlags memPropFlags,
+		VkDeviceSize minOffsetAlignment)
+		: mDevice{ device }, mInstanceSize{ instanceSize }, mInstanceCount{ instanceCount },
+		mUsageFlags{ usageFlags }, mMemoryPropertyFlags{ memPropFlags } {
+
+		mAlignmentSize = getAlignment(instanceSize, minOffsetAlignment);
+		mBufferSize = mAlignmentSize * mInstanceCount;
+		mDevice.createBuffer(mBufferSize, usageFlags, memPropFlags, mBuffer, mMemory);
+	}
+
+	Buffer::~Buffer()
+	{
+		unMap();
+		vkDestroyBuffer(mDevice.device(), mBuffer, CUSTOM_ALLOCATOR);
+		vkFreeMemory(mDevice.device(), mMemory, CUSTOM_ALLOCATOR);
+	}
+
+	VkResult Buffer::map(VkDeviceSize size, VkDeviceSize offset)
+	{
+		assert(mBuffer && mMemory && "Called map on buffer before create");
+		return vkMapMemory(mDevice.device(), mMemory, offset, size, 0, &mMapped);
+	}
+
+	void Buffer::unMap()
+	{
+		if (mMapped) {
+			vkUnmapMemory(mDevice.device(), mMemory);
+			mMapped = nullptr;
+		}
+	}
+
+	void Buffer::writeToBuffer(void* data, VkDeviceSize size, VkDeviceSize offset)
+	{
+		assert(mMapped && "Cannot write to unmapped buffer");
+
+		char* memOffset = (char*)mMapped;
+		memOffset += offset;
+		memcpy(memOffset, data, size);
+	}
+
+	VkResult Buffer::flush(VkDeviceSize size, VkDeviceSize offset)
+	{
+		VkMappedMemoryRange mappedRange = {};
+		mappedRange.sType = VK_STRUCTURE_TYPE_MAPPED_MEMORY_RANGE;
+		mappedRange.memory = mMemory;
+		mappedRange.offset = offset;
+		mappedRange.size = size;
+		return vkFlushMappedMemoryRanges(mDevice.device(), 1, &mappedRange);
+	}
+
+	VkDescriptorBufferInfo Buffer::descriptorInfo(VkDeviceSize size, VkDeviceSize offset)
+	{
+		VkDescriptorBufferInfo descBuffInfo = {};
+		descBuffInfo.buffer = mBuffer;
+		descBuffInfo.offset = offset;
+		descBuffInfo.range = size;
+		return descBuffInfo;
+	}
+
+	VkResult Buffer::invalidate(VkDeviceSize size, VkDeviceSize offset)
+	{
+		VkMappedMemoryRange mappedRange = {};
+		mappedRange.sType = VK_STRUCTURE_TYPE_MAPPED_MEMORY_RANGE;
+		mappedRange.memory = mMemory;
+		mappedRange.offset = offset;
+		mappedRange.size = size;
+		return vkInvalidateMappedMemoryRanges(mDevice.device(), 1, &mappedRange);
+	}
+
+	void Buffer::writeToIndex(void* data, int32_t idx)
+	{
+		writeToBuffer(data, mInstanceSize, idx * mAlignmentSize);
+	}
+
+	VkResult Buffer::flushToIndex(int32_t idx)
+	{
+		return flush(mAlignmentSize, idx * mAlignmentSize);
+	}
+
+	VkDescriptorBufferInfo Buffer::descriptorInfoIndex(int32_t idx)
+	{
+		return descriptorInfo(mAlignmentSize, idx * mAlignmentSize);
+	}
+
+	VkResult Buffer::invalidateIndex(int32_t idx)
+	{
+		return invalidate(mAlignmentSize, idx * mAlignmentSize);
+	}
+
+	VkDeviceSize Buffer::getAlignment(VkDeviceSize instanceSize, VkDeviceSize minOffsetAlignment)
+	{
+		if (minOffsetAlignment > 0) {
+			return (instanceSize + minOffsetAlignment - 1) & ~(minOffsetAlignment - 1);
+		}
+		return instanceSize;
+	}
+
 }
