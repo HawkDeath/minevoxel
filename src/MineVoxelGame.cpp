c1f95ca39969a40572d261e155c48b21a69526ff
HawkDeath <patryj1083@gmail.com>
2022-02-05 15:39:48 +0100

    ModelTestRenderSystem works

--- src/MineVoxelGame.cpp
+++ src/MineVoxelGame.cpp
@@ -2,38 +2,111 @@
 #include <chrono>
 #include "Log.h"
 
+#include "Descriptors.h"
+
+#include "systems/ModelTestRenderSystem.h"
 #include "systems/TestRenderSystem.h"
 #include "Model.h"
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
 namespace mv {
+  struct UniformBufferObj
+  {
+    glm::mat4 model;
+    glm::mat4 view;
+    glm::mat4 projection;
+  };
+  
+
   void MineVoxelGame::run()
   {
 
-    TestRenderSystem renderSystem = { device, renderer.getSwapChainRenderPass(), nullptr };
+    std::vector<std::unique_ptr<Buffer>> uboBuffers(SwapChain::MAX_FRAME_IN_FLIGHT);
+    auto sizeOfBuffer = static_cast<VkDeviceSize>(sizeof(UniformBufferObj));
+
+    for (int i = 0; i < uboBuffers.size(); i++) {
+      uboBuffers[i] = std::make_unique<Buffer>(device,
+        sizeOfBuffer,
+        1,
+        VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
+        VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT);
+      uboBuffers[i]->map(sizeOfBuffer);
+    }
+
+
+    std::unique_ptr<DescriptorSetLayout> globalDescriptorSetLayout = std::make_unique<DescriptorSetLayout>(device);
+    globalDescriptorSetLayout->addBinding(0, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_ALL_GRAPHICS);
+    globalDescriptorSetLayout->createDescriptorSetLayout();
+
+    std::unique_ptr<DescriptorPool> globalPool = std::make_unique<DescriptorPool>(device);
+    globalPool->setMaxSets(SwapChain::MAX_FRAME_IN_FLIGHT);
+    globalPool->addPoolSize(VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, SwapChain::MAX_FRAME_IN_FLIGHT);
+    globalPool->createDescriptorPool();
+
+    std::vector<VkDescriptorSet> globalDescriptorSets(SwapChain::MAX_FRAME_IN_FLIGHT);
+    for (int i = 0; i < globalDescriptorSets.size(); i++) {
+      auto bufferInfo = uboBuffers[i]->descriptorInfo(sizeOfBuffer);
+      DescriptorWriter writer = { *globalDescriptorSetLayout, *globalPool };
+      writer.writeBuffer(0, &bufferInfo);
+      writer.build(globalDescriptorSets[i]);
+    }
+
+    ModelTestRenderSystem renderSystem = { device, renderer.getSwapChainRenderPass(), globalDescriptorSetLayout->getDescriptorSetLayout() };
+
     ModelLoader loader;
-    loader.vertices.push_back({});
+    loader.vertices.push_back({ {0.0f, -0.5f, 0.0f, 1.0f}, {1.0f, 0.0f, 0.0f, 1.0f}, {1.0f, 1.0f, 1.0f, 1.0f}, {0.f, 0.f} });
+    loader.vertices.push_back({ {0.5f,  0.5f, 0.0f, 1.0f}, {0.0f, 1.0f, 0.0f, 1.0f}, {1.0f, 1.0f, 1.0f, 1.0f}, {0.f, 0.f} });
+    loader.vertices.push_back({ {-0.5f, 0.5f, 0.0f, 1.0f}, {0.0f, 0.0f, 1.0f, 1.0f}, {1.0f, 1.0f, 1.0f, 1.0f}, {0.f, 0.f} });
 
     std::vector<std::unique_ptr<Model>> models;
     models.push_back( std::make_unique<Model>(device, loader));
 
     auto currentTime = std::chrono::high_resolution_clock::now();
+    auto input = window.getInput();
+
+    glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+    glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+    glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
     while (!window.shouldClose())
     {
       glfwPollEvents();
 
       auto newTime = std::chrono::high_resolution_clock::now();
-      float frameTime = std::chrono::duration<float, std::chrono::seconds::period>(newTime - currentTime).count();
+      auto frameTime = std::chrono::duration<float, std::chrono::seconds::period>(newTime - currentTime).count();
 
       currentTime = newTime;
 
       // update
-      auto input = window.getInput();
       if (input->getKeyState(GLFW_KEY_ESCAPE) == GLFW_PRESS) {
         glfwSetWindowShouldClose(window.window(), GLFW_TRUE);
       }
+      auto step = 0.01f;
+      if (input->getKeyState(GLFW_KEY_W) == GLFW_PRESS) {
+        cameraPos.z -= step;
+      }
+      if (input->getKeyState(GLFW_KEY_S) == GLFW_PRESS) {
+        cameraPos.z += step;
+      }
+      if (input->getKeyState(GLFW_KEY_A) == GLFW_PRESS) {
+        cameraPos.x -= step;
+      }
+      if (input->getKeyState(GLFW_KEY_D) == GLFW_PRESS) {
+        cameraPos.x += step;
+      }
 
       // draw
-      float aspect = renderer.getAspectRatio();
+      auto aspect = renderer.getAspectRatio();
+      auto frameIdx = renderer.getFrameIndex();
+
+
+      UniformBufferObj ubo = {};
+      ubo.projection = glm::perspective(glm::radians(90.0f), (float)aspect, 0.1f, 100.0f);
+      ubo.view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+      ubo.model = glm::mat4(1.0f);
+
 
       // set aspect for camera
       if (auto commandBuffer = renderer.beginFrame()) {
@@ -41,16 +114,19 @@ namespace mv {
 
         FrameInfo frameInfo = {
           commandBuffer,
-          nullptr,
+          globalDescriptorSets[frameIdx],
           models
         };
 
         // update UBO; MVP matrix
 
-        renderer.beginSwapChainRenderPass(commandBuffer);
+        uboBuffers[frameIdx]->writeToBuffer(&ubo, sizeof(ubo));
+        uboBuffers[frameIdx]->flush(sizeof(ubo));
+
+        renderer.beginSwapChainRenderPass(frameInfo.commandBuffer);
         // render system; call to all objects to draw via vkCmdDraw()
-        renderSystem.render(commandBuffer);
-        renderer.endSwapChainRenderPass(commandBuffer);
+        renderSystem.render(frameInfo);
+        renderer.endSwapChainRenderPass(frameInfo.commandBuffer);
         renderer.endFrame();
       }
     }
